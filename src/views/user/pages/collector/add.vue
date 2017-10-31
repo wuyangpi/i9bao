485a071c1ace61693962e27c2f19322094685b5f
codehui <547758623@qq.com>
2017-10-31 22:03:43 +0800

    feat: 详细信息完善，需要查看富文本组件

--- src/views/user/pages/collector/add.vue
+++ src/views/user/pages/collector/add.vue
@@ -5,11 +5,11 @@
     <div class="wrap">
       <div class="title">价格信息</div>
       <div>
-        <div class="collect-price">
+        <div class="collect-price marb20">
           <div class="prc-title">征集价格：</div>
           <multiRadio v-model="collPrice" :radioArray="radioList1">
             <multiRadio slot="radio0" v-model="typePrice" :radioArray="radio1List">
-              <multiRadio slot="radio0" v-model="setPrice" :radioArray="radio2List">
+              <multiRadio slot="radio0" v-if="typePrice === 'once'" v-model="setPrice" :radioArray="radio2List">
                 <div slot="radio0" class="line-block font12">
                   <el-input class="line-block small-input" v-model="specificPirce"></el-input>
                   <span>元</span>
@@ -21,12 +21,13 @@
                   <span>元</span>
                 </div>
               </multiRadio>
-              <div slot="radio1" class="line-block font14 marl20 stages">
-                <div class="line-block marb20" v-for="(item, index) in periods">
+              <div slot="radio1" v-if="typePrice === 'stages'" class="line-block font14 marl20 stages">
+                <div class="line-block marr20" :class="{ mart20: index > 1 }" v-for="(item, index) in periods">
                     {{(index+1).toLocaleString('zh-Hans-CN-u-nu-hanidec')}}期付款：
                   <el-input class="line-block small-input" v-model="stagePrice[index]"></el-input>
+                  <i v-if="index > 0" @click="reducePeriods(index)" class="line-block iconfont icon-chahao"></i>
                 </div>
-                <div class="line-block addStage">+</div>
+                <div class="line-block addStage" @click="addPeriods"><i class="iconfont icon-jiahao"></i></div>
               </div>
             </multiRadio>
           </multiRadio>
@@ -40,6 +41,12 @@
       </div>
     </div>
     <!-- 价格信息结束-->
+    <!-- 详细信息-->
+    <detail title="详细信息"></detail>
+    <div class="btn-set">
+      <el-button type="primary" class="save-btn">保存</el-button>
+      <el-button type="primary" class="cancel-btn">取消</el-button>
+    </div>
   </div>
 </template>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
@@ -49,7 +56,7 @@
     .title {
       margin-bottom 20px
     }
-    .collect-price {
+    .collect-price, .logistics-info {
       display flex
       flex-flow row nowrap
       align-items center
@@ -64,33 +71,34 @@
       width 100px
       margin 0 10px
     }
-    .marl20 {
-      margin-left 20px
-    }
-    .marb20 {
-      margin-bottom 20px
-    }
     .stages {
       align-self: flex-start;
+      .icon-chahao {
+        cursor pointer
+        font-size 16px
+        color #bfcbd9
+      }
     }
     .addStage {
-      width 20px;
-      height 20px;
       text-align center
+      vertical-align middle
       cursor pointer
-      border 1px solid #ccc;
-      border-radius: 2px;
-      box-shadow: 0 0 0 1px rgba(0,0,0, 0.3);
+      .icon-jiahao {
+        font-size 25px
+        color #ff6e1b
+      }
     }
   }
 </style>
 <script>
   import baseInfo from './basic.vue'
   import multiRadio from '../../components/multi-radio.vue'
+  import detail from './detail.vue'
   export default{
     components: {
       baseInfo,
-      multiRadio
+      multiRadio,
+      detail
     },
     data() {
       return {
@@ -124,6 +132,26 @@
         ],
         periods: 5
       }
+    },
+    methods: {
+      /**
+       * 增加分期
+       */
+      addPeriods() {
+        if (this.periods < 5) {
+          this.periods++
+        }
+      },
+      /**
+       * 删除某一期
+       * @param {Number} index 删除第几期
+       */
+      reducePeriods(index) {
+        if (this.periods > 1) {
+          this.periods--
+          this.stagePrice.splice(index, 1)
+        }
+      },
     },
     watch: {
       typePrice(val, old) {
--- src/views/user/pages/collector/detail.vue
+++ src/views/user/pages/collector/detail.vue
@@ -1,17 +1,50 @@
 <template>
-  <div>
-    这是新建发布的详情页面
+  <div class="wrap">
+    <div class="title">{{title}}</div>
+    <div class="ruleDetail mart20">
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="80px">
+        <el-form-item label="商品主图" required>
+          <upload v-model="ruleForm.collectImg" :isDelete="false" prompt="请上传JPG,JPEG,PNG,PDF格式的图片"></upload>
+        </el-form-item>
+        <el-form-item label="征集详情" required>
+          <div class="detail"></div>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-
+  @import "add-common.styl"
+  .detail {
+    width 500px
+    height 200px
+    border 1px solid
+  }
 </style>
 <script>
-  export default{
+  export default {
+    props: {
+      title: {
+        type: String,
+        defalut: '标题',
+      },
+    },
     data() {
       return {
-        msg:'hello vue'
+        ruleForm: {
+          collectImg: '',
+          detail: ''
+        },
+        rules: {
+          collectImg: [
+            { required: true, message: '请输入姓名', trigger: 'blur' },
+          ],
+        },
+        priceList: [
+          { label: '指定价格', value: '0', },
+          { label: '商家报价', value: '0', },
+        ],
       }
-    },
+    }
   }
 </script>
