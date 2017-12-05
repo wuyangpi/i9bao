97be445b9c0880d49449143ffb5b1a6dbd8de7a2
shihui <123456>
2017-12-05 14:29:45 +0800

    feat: 首页页脚图标添加

--- src/components/HbFoot.vue
+++ src/components/HbFoot.vue
@@ -8,7 +8,7 @@
         <div class="right">
           <div class="wrap" v-for="data in footData">
             <div class="wrap-top">
-              <i class="el-icon-share"></i>
+              <i class="iconfont" :class="data.icon"></i>
               <span>{{data.name}}</span>
             </div>
             <div class="wrap-bottom" v-for="link in data.content">
@@ -16,7 +16,8 @@
             </div>
           </div>
           <div class="qrcode" v-for="(item, index) in qrcodeList" :key="index">
-            <div class="img" :style="{ 'background': url('item.src') }"></div>
+            <img src="../assets/images/home/qrcode.png" />
+            <!--<div class="img" v-bind:style="{ backgroundImage: 'url(' + item.src + ')' }"></div>-->
             <span>{{item.name}}</span>
           </div>
         </div>
@@ -34,26 +35,26 @@ export default {
   data() {
     return {
       footData: [
-        { name: '友情链接', icon: 'el-icon-share',
+        { name: '友情链接', icon: 'icon-youqinglianjie',
           content: [
             { text: '链接一', href: 'www.baidu.com'},
             { text: '链接二', href: 'www.baidu.com'},
             { text: '链接三', href: 'www.baidu.com'},
             {text: '饿了么官网', href: 'http://element.eleme.io/#/zh-CN/component/icon'}]},
-        { name: '我要发布征集', icon: 'el-icon-information',
+        { name: '我要发布征集', icon: 'icon-bianji',
           content: [
             { text: '如何注册账户', href: 'www.baidu.com'},
             { text: '如何发布征集', href: 'http://element.eleme.io/#/zh-CN/component/icon'},
             { text: '征集注意事项', href: 'www.baidu.com'},
           ]
         },
-        { name: '我要应征', icon: 'el-icon-information',
+        { name: '我要应征', icon: 'icon-moneybag',
           content: [
             { text: '如何参与应征', href: 'www.baidu.com'},
             { text: '如何设置店铺', href: 'http://element.eleme.io/#/zh-CN/component/icon'},
           ]
         },
-        { name: '帮助中心', icon: 'el-icon-setting',
+        { name: '帮助中心', icon: 'icon-bangzhu',
           content: [
             { text: '如何注册', href: 'www.baidu.com'},
             { text: '忘记密码/修改密码', href: 'http://element.eleme.io/#/zh-CN/component/icon'},
@@ -90,26 +91,27 @@ export default {
     color #fff
     border 1px solid #c8c7cd
     .basic-flex {
-      height 180px
       margin 10px 0 30px 0
       display: flex
       flex-flow row nowrap
       justify-content: center
       align-items: center
       .left {
-        width 400px
+        min-width 350px
         text-align center
         .title {
+          width 40px
+          margin 0 auto
           position relative
-          margin-bottom 5px
+          margin-bottom 10px
           font-size 20px
           &:after {
             position absolute
-            left 178px
-            bottom -2px
+            left 0
+            bottom -5px
             content ''
             width 40px
-            height 3px
+            height 2px
             background #fff
           }
         }
@@ -117,33 +119,37 @@ export default {
       .right {
         display flex
         flex 1
-        justify-content: flex-start
+        justify-content: center
         align-items: flex-start
         .wrap {
           position relative
           margin: 5px 30px
+          min-width: 100px
           &:after {
             position absolute
             top 4px
             right -30px
             content ''
             width 1px
-            height 160px
+            height 130px
             background #fff
           }
           .wrap-top {
-            padding-bottom 12px
+            padding-bottom 5px
           }
           .wrap-bottom {
-            margin 12px 0
+            font-size 12px
+            margin 6px 0
           }
         }
         .qrcode {
-          width 150px
-          height 150px
-          background-repeat no-repeat
-          background-position  0 0
-          background-size contain
+          font-size 12px
+          margin 30px 20px
+          img {
+            widht 70px
+            height 70px
+            display block
+          }
         }
       }
     }
