d3883109baa7fee90475462d99d85bf3afb7a8e0
shihui <123456>
2017-12-10 16:44:33 +0800

    feat: 店铺分类

--- src/views/user/pages/shop/category.vue
+++ src/views/user/pages/shop/category.vue
@@ -1,22 +1,25 @@
 <!-- 设置店铺分类 -->
 <template>
   <div>
-    <el-cascader
-      placeholder="搜索分类"
-      v-model="selectedCate"
-      :clearable="true"
-      :options="categoryList"
-      filterable
-      change-on-select
-    ></el-cascader>
-    <el-button type="primary" @click="addCate">添加</el-button>
-    <div class="had-cate">
-      <span>已添加分类</span>
-      <div>
+    <div class="choose-category">
+      <div class="choose">
+        <span class="high">请选择添加您的分类</span>
+        <el-tree
+          :data="data4"
+          :props="defaultProps"
+          :highlight-current="true"
+          show-checkbox
+          node-key="id"
+          default-expand-all
+          @check-change="checkChange"
+          :expand-on-click-node="false">
+        </el-tree>
+      </div>
+      <div class="had-cate">
+        <span class="high">您选中的分类</span>
         <el-tree
           :data="categoryTree"
           :props="defaultProps"
-          show-checkbox
           node-key="id"
           default-expand-all
           :expand-on-click-node="false"
@@ -24,13 +27,64 @@
         </el-tree>
       </div>
     </div>
-    <div>
-      <span>添加自定义分类</span>
-      <div></div>
+    <div class="choose-category">
+      <div class="choose">
+        <span class="high">请添加您的自定义分类</span>
+        <div>
+          <el-input class="cate-input" v-model="selfCategory"></el-input>
+        </div>
+        <el-button class="cate-btn" type="primary" @click="addCate">添加</el-button>
+      </div>
+      <div class="had-cate">
+        <span class="high">您选中的自定义分类</span>
+        <el-tree
+          :data="selfCategoryList"
+          :props="defaultProps"
+          node-key="id"
+          default-expand-all
+          :expand-on-click-node="false"
+          :render-content="renderContent">
+        </el-tree>
+      </div>
     </div>
   </div>
 </template>
+<style lang="stylus">
+  .choose-category {
+    .choose {
+      .cate-btn {
+        width 250px
+      }
+    }
+  }
+</style>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
+  .choose-category {
+    display flex
+    flex-flow row nowrap
+    justify-content flex-start
+    >div {
+      min-width 320px
+      margin 10px
+      padding 10px
+      border 1px solid #ccc
+      >span {
+        margin-bottom 10px
+      }
+    }
+    .cate-input {
+      margin 10px
+      width 250px
+    }
+    .choose {
+      .cate-btn {
+        margin 10px
+      }
+    }
+  }
+  .high {
+    color:#ff6e1b
+  }
 .had-cate {
   margin 20px
 }
@@ -41,9 +95,18 @@
   export default {
     data() {
       return {
+        // 自定义分类
+        selfCategoryList: [],
+        selfCategory: '',
+        // 已有分类
         selectedCate: [],
         categoryList: categoryList,
         categoryTree: [],
+        chooseTree: [],
+        chooseTreeLen: 0, // 总数
+        checklen: 0, // 实时变化的总数
+        chooseId: [],
+        deleteTree: [],
         data4: [{
           id: 1,
           label: '一级 1',
@@ -85,27 +148,97 @@
         }
       }
     },
+    computed: {
+      categoryId() {
+        const arr = []
+        for (let i = 0; i< this.categoryTree.length; i++) {
+          arr.push(this.categoryTree[i].id)
+        }
+        return arr
+      },
+      hadcategoryId() {
+        const arr = []
+        for (let i = 0; i< this.data4.length; i++) {
+          arr.push(this.data4[i].id)
+        }
+        return arr
+      },
+    },
     methods: {
       // 应该向后端请求数据
       addCate() {
-        console.log(this.selectedCate)
         const id = Math.random(0, 200)
-        this.categoryTree.push({ id, label: this.selectedCate[0], children: []})
-      },
-      append(data) {
-        this.$nextTick(() => {
-          const newChild = { id: data.id++, label: 'testtest', children: [] };
-          if (!data.children) {
-            this.$set(data, 'children', []);
-          }
-          data.children.push(newChild);
-        })
+        this.selfCategoryList.push({ id, label: this.selfCategory, children: []})
       },
       remove(node, data) {
         const parent = node.parent;
         const children = parent.data.children || parent.data;
         const index = children.findIndex(d => d.id === data.id);
         children.splice(index, 1);
+      },
+      checkChange(data, checked) {
+        if (this.chooseTreeLen === 0) {
+          const obj = JSON.parse(JSON.stringify(data))
+          this.chooseTree.push(obj)
+          this.chooseTreeLen = 1
+          const childArr = data.children
+          if (childArr) {
+            this.chooseTreeLen += childArr.length
+            for (let i = 0; i < childArr.length; i++) {
+              if (childArr[i].children) {
+                this.chooseTreeLen += childArr[i].children.length
+              }
+            }
+          }
+        }
+        if (checked) {
+          this.chooseId.push(data.id)
+        }
+        this.checklen++
+        if ((this.chooseTreeLen === this.checklen)
+        || (this.chooseTreeLen === this.checklen + 1)) {
+          this.checklen = 0
+          this.chooseTreeLen = 0
+          const arr = this.getArray(this.chooseTree, this.chooseId)
+          if (arr.length > 0) {
+            const index = this.categoryId.indexOf(arr[0].id)
+            const hadIndex = this.hadcategoryId.indexOf(arr[0].id)
+            if (index > -1) {
+              this.$set(this.categoryTree, index, arr[0])
+            } else if (hadIndex > -1){
+              this.categoryTree = this.categoryTree.concat(arr)
+            }
+          } else {
+            const hindex = this.getIndex(this.categoryTree, data.id)
+            this.$delete(this.categoryTree, hindex)
+          }
+          this.chooseTree.length = 0
+          this.chooseId.length = 0
+        }
+      },
+      getArray(arr, ids) {
+        for (let i = 0; i < arr.length; i++) {
+          if (ids.indexOf(arr[i].id) === -1) {
+            if (arr[i].children) {
+              this.getArray(arr[i].children, ids)
+            } else {
+              arr.splice(i, 1)
+            }
+          }
+        }
+        return arr
+      },
+      getIndex(arr, id) {
+        let index = -1
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i].id !== id) {
+            if (arr[i].children && this.getIndex(arr[i].children, id) === -1) {
+              break
+            }
+          }
+          index = i
+        }
+        return index
       },
       renderContent(h, { node, data, store }) {
         return (
@@ -114,7 +247,6 @@
               <span>{node.label}</span>
             </span>
             <span>
-              <el-button style="font-size: 12px;" type="text" on-click={ () => this.append(data) }>修改</el-button>
               <el-button style="font-size: 12px;" type="text" on-click={ () => this.remove(node, data) }>删除</el-button>
             </span>
           </span>
