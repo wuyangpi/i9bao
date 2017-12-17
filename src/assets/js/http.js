dfcf3aa89a13fd3c470be82bea35b64490a6d830
shihui <123456>
2017-12-17 22:20:55 +0800

    feat: 全局注册http事件

--- src/assets/js/http.js
+++ src/assets/js/http.js
@@ -20,10 +20,46 @@ methods.forEach(method => {
   http[method] = axios[method].bind(axios)
 })
 
-export default http
+export default {
+  /**
+   * install钩子
+   * @param {Vue} Vue Vue
+   */
+  install(Vue) {
+    Vue.prototype.http = http
+  },
+}
 
 export const addRequestInterceptor =
   axios.interceptors.request.use.bind(axios.interceptors.request)
 
 export const addResponseInterceptor =
   axios.interceptors.response.use.bind(axios.interceptors.response)
+
+// 接口前自动添加api配置
+addRequestInterceptor(
+  (config) => {
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+addResponseInterceptor(
+  (response) => {
+    // 在这里统一前置处理请求响应
+    // if (Number(response.data.code) !== 0) {
+      // 全局notify有问题，还是自己处理吧
+    //   return Promise.reject(response.data)
+    // }
+    return Promise.resolve(response.data)
+  },
+  (error) => {
+    // 401直接跳转到登陆页面 如何登陆状态过期了怎么办？
+    // if (!IS_DEBUG && Number(error.response.status) === 401) {
+    //   httpEvent.emit('LOGIN_TIMEOUT')
+    // }
+    return Promise.reject(error || '出错了')
+  }
+)
