df5131cdba0d249a165afca23eafe7e302501c28
shihui <123456>
2017-12-19 17:56:55 +0800

    feat: 手机校验码接口公共出来

--- src/views/login/find-pass.vue
+++ src/views/login/find-pass.vue
@@ -33,8 +33,10 @@
 </template>
 <script type="text/babel">
   import validates from './validate'
+  import mixinCommon from './common'
 
   export default {
+    mixins: [mixinCommon],
     data(){
       const validatePwdagain = (rule, value, callback) => {
         if (value === '') {
@@ -67,49 +69,9 @@
             { required: true, message: '请输入验证码', trigger: 'blur' },
           ],
         },
-        verticalText: '获取验证码',
-        hadVertical: false, // 是否已经获取验证码，60到期重新获取
-        reduceSec: 60,
       }
     },
     methods: {
-      // 获得验证码
-      getVerticalCode() {
-        const _this = this
-        let text = ''
-        this.$refs.ruleForm.validateField('phone', (err) => {
-          text = err
-        })
-        if (text === '') {
-          this.http.post('/rest/common/sms', { phone: this.ruleForm.phone }).then((res) => {
-            if (res.result === 1) {
-              this.$message({
-                message: '发送成功！',
-                type: 'success'
-              })
-            } else {
-              this.$message({
-                message: res.message || '发送失败！',
-                type: 'error'
-              })
-            }
-          })
-          if (!this.hadVertical) {
-            _this.hadVertical = true
-            this.verticalText = this.reduceSec + '秒'
-            let time = setInterval(function () {
-              _this.reduceSec--
-              _this.verticalText = _this.reduceSec + '秒'
-              if (_this.reduceSec <= 0) {
-                _this.hadVertical = false
-                _this.reduceSec = 60
-                _this.verticalText = '获取验证码'
-                clearInterval(time)
-              }
-            }, 1000)
-          }
-        }
-      },
       submitForm(formName){
         this.$refs[formName].validate((valid) => {
           if (valid) {
--- src/views/login/signup.vue
+++ src/views/login/signup.vue
@@ -43,9 +43,10 @@
 </template>
 <script type="text/babel">
   import validates from './validate'
+  import mixinCommon from './common'
 
   export default{
-    mixins: [validates],
+    mixins: [mixinCommon],
     data() {
       const validatePwdagain = (rule, value, callback) => {
         if (value === '') {
@@ -83,49 +84,9 @@
           ],
         },
         advertise: require('../../assets/images/home/signup.png'),
-        verticalText: '获取验证码',
-        hadVertical: false, // 是否已经获取验证码，60到期重新获取
-        reduceSec: 60,
       }
     },
     methods: {
-      // 获得验证码
-      getVerticalCode() {
-        const _this = this
-        let text = ''
-        this.$refs.ruleForm.validateField('phone', (err) => {
-          text = err
-        })
-        if (text === '') {
-          this.http.post('/rest/common/sms', { phone: this.ruleForm.phone }).then((res) => {
-            if (res.result === 1) {
-              this.$message({
-                message: '发送成功！',
-                type: 'success'
-              })
-            } else {
-              this.$message({
-                message: res.message || '发送失败！',
-                type: 'error'
-              })
-            }
-          })
-          if (!this.hadVertical) {
-            _this.hadVertical = true
-            this.verticalText = this.reduceSec + '秒'
-            let time = setInterval(function () {
-              _this.reduceSec--
-              _this.verticalText = _this.reduceSec + '秒'
-              if (_this.reduceSec <= 0) {
-                _this.hadVertical = false
-                _this.reduceSec = 60
-                _this.verticalText = '获取验证码'
-                clearInterval(time)
-              }
-            }, 1000)
-          }
-        }
-      },
       submitForm(formName){
         this.$refs[formName].validate((valid) => {
           if (valid) {
